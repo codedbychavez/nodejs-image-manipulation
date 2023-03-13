f6d44000c1a07ad608941012bf1d1c1d6594b7d8
greggOD <info@greggsworld.co.uk>
2023-03-13 10:55:53 +0200

    Update: Processing additional images

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const sharp = require("sharp");
 const { getFileName } = require("./utils/utility-functions");
 
+const IMAGE_LIST = [
+  'image-1.jpg',
+  'image-2.jpg',
+  'image-3.jpg',
+  'image-4.webp',
+  'image-5.jpg',
+  'image-6.webp',
+  'image-7.jpg',
+  'image-8.jpg',
+  'image-9.jpg',
+  'image-10.jpg',
+  'image-11.jpg',
+  'image-12.gif',
+]
+
 function trimImage(imageFilePath) {
   const fileName = getFileName(imageFilePath);
-
+  
   return sharp(imageFilePath)
     .trim({
       background: "white",
       threshold: 11,
     })
-    .toFile(`output/trimmed_${fileName}`);
+    .toFile(`./output/trimmed_${fileName}`);
 }
 
-trimImage("input/image.jpg");
+IMAGE_LIST.map((path)=> {
+  trimImage(`input/${path}`);
+})
